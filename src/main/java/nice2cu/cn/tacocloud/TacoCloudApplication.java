5f678656d46b1379db5b72c20dc27971b634af07
zhaosq <zhaosq@seirobotics.net>
2021-05-13 15:59:50 +0800

    Initial commit

---
+++ src/main/java/nice2cu/cn/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package nice2cu.cn.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nice2cu/cn/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package nice2cu.cn.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
